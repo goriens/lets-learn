64825719b8b29c2ba5c2ba52b00738e1b7b874e8
Suhaib Gour <76823175+goriens@users.noreply.github.com>
2024-09-09 20:32:02 +0530

    all

--- app/study/page.tsx
+++ app/study/page.tsx
@@ -1,6 +1,3 @@
-import { redirect } from "next/navigation";
-
 export default function page() {
-  redirect("/study/notes");
   return <div>Study redirect</div>;
 }
--- data/navlinks.ts
+++ data/navlinks.ts
@@ -1,5 +1,5 @@
 export const navLinks = [
-  { id: 3, name: "Notes", link: "/study/notes" },
+  { id: 3, name: "Notes", link: "/study" },
   { id: 4, name: "Final Grade", link: "/final-grade" },
   { id: 6, name: "Groups", link: "/groups" },
   { id: 7, name: "About", link: "/about" },
